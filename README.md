8efc92a86dc0a90c633baddd7966b234495a2048
joe_yang <yo2man0929@gmail.com>
2021-11-07 20:39:41 +0800

    Onboarding the binary

---
+++ README.md
@@ -0,0 +1,85 @@
+# A instrumatation tool for http request
+
+--
+
+### git clone the project then
+
+Build linux version
+
+`cd http-get-tool; env GOOS=linux GOARCH=amd64 go build .`
+
+Build darwain version
+`cd http-get-tool; go build .`
+
+Build windows version
+`cd http-get-tool; env GOOS=windwos GOARCH=amd64 go build .`
+
+```
+./http-get-tool -h                                                                                                                     (gke_devops-mgmt-330303_asia-east1_devops-mgmt/default)
+
+Get Help infomation
+Eg. http-get-tool -h
+
+Query the target one time and get the content
+Eg. http-get-tool -u http://ipinfo.io
+    http-get-tool --url http://ioinfo.io
+
+Query the target with multithreads and get some instrumentation
+Eg. http-get-tool --url http://www.google.com --profile 10
+    http-get-tool -u http://www.google.com -p 10
+
+Usage:
+  http-get-tool [flags]
+
+Flags:
+  -h, --help             help for http-get-tool
+  -p, --profile string   how many times of your request (default "1")
+  -u, --url string       your target website
+
+```
+
+![help](./help.png)
+
+```
+./http-get-tool -u http://ipinfo.io
+
+2021/11/07 20:26:23 issuing GET to URL: http://ipinfo.io
+2021/11/07 20:26:23 Total duration: 450.456412ms
+Status code 200.
+Size is 294 bytes.
+Content is as below.
+__________
+{
+  "ip": "60.251.54.196",
+  "hostname": "60-251-54-196.hinet-ip.hinet.net",
+  "city": "Taipei",
+  "region": "Taiwan",
+  "country": "TW",
+  "loc": "25.0478,121.5319",
+  "org": "AS3462 Data Communication Business Group",
+  "timezone": "Asia/Taipei",
+  "readme": "https://ipinfo.io/missingauth"
+}
+```
+
+![conten](./content.png)
+
+```
+http-get-tool -u http://ipinfo.io -p 3
+
+2021/11/07 20:26:28 issuing GET to URL: http://ipinfo.io
+2021/11/07 20:26:28 issuing GET to URL: http://ipinfo.io
+2021/11/07 20:26:28 issuing GET to URL: http://ipinfo.io
+2021/11/07 20:26:29 Total duration: 447.251062ms
+2021/11/07 20:26:29 Total duration: 479.796688ms
+2021/11/07 20:26:29 Total duration: 505.151084ms
+The sucessful query rate is  100%!
+(MISSING)The mean response time 477.399611ms.
+The median response time 479.796688ms.
+The fastest reponse time 447.251062ms.
+TThe size in bytes of the smallest response 294 bytes.
+The size in bytes of the largest response 294 bytes.
+
+```
+
+![parallel-query](./parallel-query.png)
--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 
@@ -70,7 +71,10 @@ func (tr *customTransport) Duration() time.Duration {
 	return tr.reqEnd.Sub(tr.reqStart)
 }
 
-func doGetUseChan(url string, statusChan chan<- int, totalDurationChan chan<- time.Duration, bodyChan chan<- []byte, sizeChan chan<- int) error {
+func doGetUseChan(url string,
+	statusChan chan<- int,
+	totalDurationChan chan<- time.Duration, bodyChan chan<- []byte,
+	sizeChan chan<- int) error {
 
 	tp := newTransport()
 	client := &http.Client{Transport: tp}
@@ -101,35 +105,69 @@ func doGetUseChan(url string, statusChan chan<- int, totalDurationChan chan<- ti
 	return nil
 }
 
-//The number of requests
-//The fastest time
-//The slowest time
-//The mean & median times
-//The percentage of requests that succeeded
-//Any error codes returned that weren't a success
-//The size in bytes of the smallest response
-//The size in bytes of the largest response
+func MinMax(array []time.Duration) (time.Duration, time.Duration) {
+	var max time.Duration = array[0]
+	var min time.Duration = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
+func successRate(statuschan chan int, intVar int) int {
+	var statuscode []int
+	for x := 0; x < intVar; x++ {
+		statuscode = append(statuscode, <-statuschan)
+	}
+	var success int = 0
+	for _, status := range statuscode {
+		if status == 200 {
+			success += 1
+		}
+	}
+	successrate := success / len(statuscode) * 100
+	return successrate
+}
+
+type ByDuration []time.Duration
+
+func (a ByDuration) Len() int           { return len(a) }
+func (a ByDuration) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByDuration) Less(i, j int) bool { return a[i] < a[j] }
+
 var rootCmd = &cobra.Command{
 	Use:   "http-get-tool",
 	Short: "Get URL repsponse annd information",
 	Long: `
+Get Help infomation
+Eg. http-get-tool -h
 
+Query the target one time and get the content
+Eg. http-get-tool -u http://ipinfo.io
+    http-get-tool --url http://ioinfo.io
+
+Query the target with multithreads and get some instrumentation 
 Eg. http-get-tool --url http://www.google.com --profile 10
-	
+    http-get-tool -u http://www.google.com -p 10
 	`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// If requested, enable logging.
 		if (url != "" && profile == "1") || (url != "" && profile == "") {
-			statusch := make(chan int, 1)
-			infoch := make(chan time.Duration, 1)
-			bodych := make(chan []byte, 1)
-			sizech := make(chan int, 1)
+			statuschan := make(chan int, 1)
+			timechan := make(chan time.Duration, 1)
+			bodychan := make(chan []byte, 1)
+			sizechan := make(chan int, 1)
 			//body := make(chan []byte)
-			doGetUseChan(url, statusch, infoch, bodych, sizech)
+			doGetUseChan(url, statuschan, timechan, bodychan, sizechan)
 
-			status := <-statusch
-			size := <-sizech
-			body := string(<-bodych)
+			status := <-statuschan
+			size := <-sizechan
+			body := string(<-bodychan)
 
 			fmt.Printf("Status code %d.\n", status)
 			fmt.Printf("Size is %d bytes.\n", size)
@@ -137,20 +175,43 @@ Eg. http-get-tool --url http://www.google.com --profile 10
 
 		} else {
 			intVar, err := strconv.Atoi(profile)
-			status := make(chan int, intVar)
-			info := make(chan time.Duration, intVar)
-			body := make(chan []byte, intVar)
-			size := make(chan int, intVar)
+			statuschan := make(chan int, intVar)
+			timechan := make(chan time.Duration, intVar)
+			bodychan := make(chan []byte, intVar)
+			sizechan := make(chan int, intVar)
 			if err != nil {
 				return fmt.Errorf("wrong profile setting %s", err.Error())
 			}
 
 			for x := 0; x < intVar; x++ {
-				go doGetUseChan(url, status, info, body, size)
+				go doGetUseChan(url, statuschan, timechan, bodychan, sizechan)
 			}
-			for data := range info {
-				fmt.Println(data)
+
+			// Get a slice of time.Duration for doing math
+			var a []time.Duration
+			for x := 0; x < intVar; x++ {
+				a = append(a, <-timechan)
 			}
+			// sort the slice to get max, min, and mid
+			sort.Sort(ByDuration(a))
+
+			// Get average response time
+			var sum int
+			for _, b := range a {
+				sum += int(b)
+			}
+			average := time.Duration(sum / len(a))
+			midvalue := a[int(len(a)/2)]
+			bodysize := <-bodychan
+
+			successrate := successRate(statuschan, intVar)
+
+			fmt.Printf("The sucessful query rate is  %d%.\n", successrate)
+			fmt.Printf("The mean response time %s.\n", average)
+			fmt.Printf("The median response time %s.\n", midvalue)
+			fmt.Printf("The fastest reponse time %s.\n", a[0])
+			fmt.Printf("TThe size in bytes of the smallest response %d bytes.\n", len(bodysize))
+			fmt.Printf("The size in bytes of the largest response %d bytes.\n", len(bodysize))
 
 		}
 		return nil
